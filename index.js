51853cf8a21bea6114f2c7dcb3f7a498ded68b26
alasiri <austinalasiri44@gmail.com>
2025-03-16 12:00:53 +0300

    Finished my work

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
 const dodger = document.getElementById('dodger');
+const game = document.querySelector('#game');
+
+
+
+
+function moveDodger(){
+    document.addEventListener('keydown', function(e){
+        switch(e.key){
+            case 'ArrowLeft':
+                moveDodgerLeft();
+                break;
+            case 'ArrowRight':
+                moveDodgerRight();
+                break;
+            case 'ArrowUp':
+                moveDodgerUp();
+                break;
+            case 'ArrowDown':
+                moveDodgerDown();
+                break;
+            default:
+                console.log('There is no such thing');
+                
+        };
+    });
+}
+
+
+function moveDodgerUp(){
+    const bottomMovement = dodger.style.bottom.replace('px','');
+    const bottom = parseInt(bottomMovement, 10);
+
+   if(bottom < game.clientHeight - dodger.clientHeight){
+    dodger.style.bottom = `${bottom + 1}px`;
+   }
+    
+}
+
+function moveDodgerDown(){
+    const bottomMovement = dodger.style.bottom.replace('px','');
+    const bottom = parseInt(bottomMovement, 10);
+
+    if(bottom > 0){
+    dodger.style.bottom = `${bottom - 1}px`;
+    }
+}
 
 
 function moveDodgerLeft(){
@@ -6,58 +52,17 @@ function moveDodgerLeft(){
     const left = parseInt(leftNumber, 10);
 
     if(left > 0){
-        dodger.style.left = `${left - 3}px`;
+    dodger.style.left = `${left - 1}px`;
     }
-};
+}
 
 function moveDodgerRight(){
-    const number = dodger.style.left.replace('px','');
-    const myNumber = parseInt(number, 10);
+    const leftNumber = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumber, 10);
 
-   
-        dodger.style.left = `${myNumber + 3}px`;
-    
-};
+    if(left < 360){
+    dodger.style.left = `${left + 1}px`;
+    } 
+}
 
-function moveDodgerUp(){
-    const number = dodger.style.bottom.replace('px','');
-    const myNumber = parseInt(number, 10);
-
-   if(myNumber > 0){
-        dodger.style.bottom = `${myNumber + 3}px`;
-   }
-    
-};
-
-function moveDodgerDown(){
-    const number = dodger.style.bottom.replace('px','');
-    const myNumber = parseInt(number, 10);
-
-    if(myNumber > 0){
-        dodger.style.bottom = `${myNumber - 3}px`;
-    }    
-};
-
-
-document.addEventListener('keydown', function(e) {
-   switch(e.key){
-   case 'ArrowLeft':
-        moveDodgerLeft();
-        break;
-
-    case 'ArrowRight':
-        moveDodgerRight();
-        break;
-
-    case 'ArrowUp':
-        moveDodgerUp();
-        break;
-
-    case 'ArrowDown':
-        moveDodgerDown();
-        break;
-
-    default:
-        console.log('Nothing reallly happens');
-   }
-});
+moveDodger();
